ad964421897ee7571a9ec079334a0cf17c996ea5
Administrator <Administrator@USER-20160504XD>
2017-11-20 12:00:00 +0800

    1.1.6

--- SSHMaven/src/main/java/com/ssh/service/ManagerService.java
+++ SSHMaven/src/main/java/com/ssh/service/ManagerService.java
@@ -2,12 +2,42 @@ package com.ssh.service;
 
 import java.util.List;
 
+import com.ssh.model.Person;
 import com.ssh.model.User;
 
 public interface ManagerService {
-	// 添加用户
+
+	// 按星期查询三个week方法
+	public List selectByWeek();
+
+	// 按月查询两个mouth方法
+	public List selectByMonth(int month);
+
+	// .添加职员
 	public void addUser(User u);
-	
-	public List orderSuccessPercent(int id) ;
+
+	// 修改职员信息
+	public void updateUser(User u);
+
+	// 删除职员
+	public void delUser(int id);
+
+	// .模糊查询所有职员
+	public List selectAllUser();
+
+	// .模糊查询所有候选人（候选人，指定时间，候选人状态）
+	public List selectAllPerson(Person p, String minDate, String maxDate);
+
+	// .预约成功率=候选人预约id为当前助理/候选人预约id为当前助理且上门时间不为空
+	public List orderSuccessPercent(int id);
+
+	// .面试成功率=当前人事已签订/当前人事所有面试候选人
+	public List interviewSuccessPercent(int id);
+
+	// .导出表格
+	public String excel(String str);
+
+	// 按id查出一个职员
+	public List selectOneUser(int id);
 
 }
--- SSHMaven/src/main/java/com/ssh/serviceImpl/ManagerServiceImpl.java
+++ SSHMaven/src/main/java/com/ssh/serviceImpl/ManagerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ssh.serviceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssh.dao.ManagerDao;
+import com.ssh.model.Person;
 import com.ssh.model.User;
 import com.ssh.service.ManagerService;
 
@@ -23,5 +25,69 @@ public class ManagerServiceImpl implements ManagerService {
 	public List orderSuccessPercent(int id) {
 		List orderSuccessPercent = ManagerDaoImpl.orderSuccessPercent(id);
 		return orderSuccessPercent;
+	}
+
+	@Override
+	public List selectByWeek() {
+		List list=new ArrayList();
+		
+		List selectOrderByWeek = ManagerDaoImpl.selectOrderByWeek();
+		List selectVisitByWeek = ManagerDaoImpl.selectVisitByWeek();
+		List selectSignByWeek = ManagerDaoImpl.selectSignByWeek();
+		list.add(selectOrderByWeek.get(0));
+		list.add(selectVisitByWeek.get(0));
+		list.add(selectSignByWeek.get(0));
+		return list;
+	}
+
+	@Override
+	public List selectByMonth(int month) {
+		List list=new ArrayList();
+		for(int i=1;i<=12;i++) {
+			List signbyMonth = ManagerDaoImpl.selectSignByMonth(month);
+			list.add(signbyMonth);
+			System.out.println("第"+i+"个月,"+"有"+signbyMonth+"个人签单");
+		} 
+		return list;	 
+	}
+
+	@Override
+	public void updateUser(User u) {
+		ManagerDaoImpl.updateUser(u);		
+	}
+
+	@Override
+	public void delUser(int id) {
+		ManagerDaoImpl.delUser(id);		
+	}
+
+	@Override
+	public List selectAllUser() {
+		List selectAllUser = ManagerDaoImpl.selectAllUser();
+		return selectAllUser;
+	}
+
+	@Override
+	public List selectAllPerson(Person p, String minDate, String maxDate) {
+		List selectAllPerson = ManagerDaoImpl.selectAllPerson(p, minDate, maxDate);
+		return selectAllPerson;
+	}
+
+	@Override
+	public List interviewSuccessPercent(int id) {
+		List interviewSuccessPercent = ManagerDaoImpl.interviewSuccessPercent(id);
+		return interviewSuccessPercent;
+	}
+
+	@Override
+	public String excel(String str) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List selectOneUser(int id) {
+		List selectOneUser = ManagerDaoImpl.selectOneUser(id);
+		return selectOneUser;
 	}
 }
